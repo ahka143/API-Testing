e2bc3a065e36c784101b1437fd7f9df4027c9a09
Ahmet Kaya <ahka1143@gmail.com>
2022-07-02 22:42:53 +0300

    02.07.22

--- src/test/java/Get01.java
+++ src/test/java/Get01.java
@@ -1,3 +1,8 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class Get01 {
     /*
     POSTMAN, manuel olarak API testi yapmak için kullanılır.
@@ -20,7 +25,32 @@ public class Get01 {
             3) Type code to send request (talep göndermek için kod yazma)
             4) Do Assertion (doğrulama yapma)
      */
-    public static void main(String[] args) {
+
+
+    /*
+    Given
+            https://restful-booker.herokuapp.com/booking/3
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be JSON
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
+   @Test
+    public void get01(){
+
+       // 1) Set the URL (URl'yi kurmak)
+       String url="https://restful-booker.herokuapp.com/booking/555";
+       //  2) Set the expected Data (beklenen datanın oluşturulması)   """POST, PUT PATCH"""
+       //  3) Type code to send request (talep göndermek için kod yazma)
+       Response response=given().when().get(url);
+       response.prettyPrint();
+       //   4) Do Assertion (doğrulama yapma)
+       response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
 
     }
 
