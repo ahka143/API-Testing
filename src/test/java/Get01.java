f71bedb7082d18e67a79fdb1a5d715d50a891a24
Ahmet Kaya <ahka1143@gmail.com>
2022-07-02 21:50:59 +0300

    02.07.22

--- src/test/java/Get01.java
+++ src/test/java/Get01.java
@@ -1,5 +1,27 @@
 public class Get01 {
+    /*
+    POSTMAN, manuel olarak API testi yapmak için kullanılır.
+    API otomasyonda ise REST Assured Library kullanacağız
+    REST API kullanmamızın sebebi hem Json hem de Xml olarak kullanılabiliyor olmasıdır.
+    Otomasyon kodlarının yazımı için şu adımları izliyoruz;
 
+    a) -->  Gereksinimleri anlama
+    b) -->  Test case'leri yazma
+    _______ Test case yazımı için Gherkin plugin kullanılacaktır.
+    _______ Given, Then, And, But gibi keywodler kullanılacaktır.
+    _______ Given (ön koşullar)
+    ________ When (aksiyonlar)
+    ________ Then (dönütler, doğrulama, response gibi)
+    ________ And (çoklu işlemeler için)
 
+    c) -->  Otomasyonda test kodunun yazımı yapılacak
+            1) Set the URL (URl'yi kurmak)
+            2) Set the expected Data (beklenen datanın oluşturulması)   """POST, PUT PATCH"""
+            3) Type code to send request (talep göndermek için kod yazma)
+            4) Do Assertion (doğrulama yapma)
+     */
+    public static void main(String[] args) {
+
+    }
 
 }
