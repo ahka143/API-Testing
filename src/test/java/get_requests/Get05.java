18186562e60ff635e64137a54e72dc42cd7ad8a2
Ahmet Kaya <ahka1143@gmail.com>
2022-07-06 23:04:44 +0300

    6.7.22

--- src/test/java/get_requests/Get05.java
+++ src/test/java/get_requests/Get05.java
@@ -1,6 +1,13 @@
 package get_requests;
 
-public class Get05 {
+import base_Urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get05 extends HerOkuAppBaseUrl {
 
     /*
         Given
@@ -10,6 +17,26 @@ public class Get05 {
         Then
             Status code is 200
 	  	And
-	  		Among the data there should be someone whose firstname is "Adamz" and last name is "Dear"
+	  		Among the data there should be someone whose firstname is "Aaron" and last name is "Chen"
      */
+
+    @Test
+    public void get01() {
+        //1.Set the Url
+
+        spec.pathParam("first","booking").queryParams("firstname","Aaron","lastname","Chen");
+
+    //2.Set the expected data
+        //3.Send the request and get the response
+     Response response= given().spec(spec).when().get("{first}");
+     response.prettyPrint();
+
+     //4.Do assertion
+
+        response.then().assertThat().statusCode(200);
+        assertTrue(response.asString().contains("1649"));
+
+
+
+    }
 }
