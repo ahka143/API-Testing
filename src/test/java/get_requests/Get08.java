5a31bea0fe62734632c2f841ddca56c625ec133d
Ahmet Kaya <ahka1143@gmail.com>
2022-07-13 21:47:06 +0300

    13.7.22

---
+++ src/test/java/get_requests/Get08.java
@@ -0,0 +1,79 @@
+package get_requests;
+
+import base_Urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get08 extends JsonPlaceHolderBaseUrl {
+    /*
+         Given
+            https://jsonplaceholder.typicode.com/todos/2
+        When
+            I send GET Request to the URL
+        Then
+            Status code is 200
+            And "completed" is false
+            And "userId" is 1
+            And "title" is "quis ut nam facilis et officia qui"
+            And header "Via" is "1.1 vegur"
+            And header "Server" is "cloudflare"
+            {
+                "userId": 1,
+                "id": 2,
+                "title": "quis ut nam facilis et officia qui",
+                "completed": false
+            }
+     */
+
+    @Test
+    public void get01() {
+        //1.set the url
+        spec.pathParams("first", "todos", "second", 2);
+        //2.set the expected data
+
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("userId", 1);
+        expectedData.put("title", "quis ut nam facilis et officia qui");
+        expectedData.put("completed", false);
+        expectedData.put("StatusCode", 200);
+        expectedData.put("Via", "1.1 vegur");
+        expectedData.put("Server", "cloudflare");
+
+        //Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        Map<String, Object> actualData = response.as(HashMap.class);
+
+        //Do assertion
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(expectedData.get("StatusCode"),response.getStatusCode());
+        assertEquals(expectedData.get("Via"),response.getHeader("Via"));
+        assertEquals(expectedData.get("Server"),response.getHeader("Server"));
+
+    }
+
+    @Test
+    public void get02() {
+        //1.set the url
+        spec.pathParams("first", "todos", "second", 2);
+        //2.set the expected data
+
+
+        //3.Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println(actualData);
+
+        //4.Do assertion
+
+
+    }
+}
