0b7d4cb7a12d916f89993804e4f69059cd2d9d5c
Ahmet Kaya <ahka1143@gmail.com>
2022-07-13 23:18:05 +0300

    13.7.22

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
 
 public class Get09 extends HerOkuAppBaseUrl {
 
@@ -35,25 +36,34 @@ public class Get09 extends HerOkuAppBaseUrl {
     public void get01() {
         //1.Set the Url
 
-        spec.pathParams("first","booking","second",91);
+        spec.pathParams("first", "booking", "second", 91);
 
         //2.Set the expectedData
-        Map<String,String> bookingDatesMap=new HashMap<>();
-        bookingDatesMap.put("checkin","2018-01-01");
-        bookingDatesMap.put("checkout","2019-01-01");
-        Map<String,Object> expectedData=new HashMap<>();
-        expectedData.put("firstname","James");
-        expectedData.put("lastname","Brown");
-        expectedData.put("totalprice",111);
-        expectedData.put("depositpaid",true);
-        expectedData.put("bookingdates",bookingDatesMap);
-        expectedData.put("additionalneeds","Breakfast");
+        Map<String, String> bookingDatesMap = new HashMap<>();
+        bookingDatesMap.put("checkin", "2018-01-01");
+        bookingDatesMap.put("checkout", "2019-01-01");
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("firstname", "James");
+        expectedData.put("lastname", "Brown");
+        expectedData.put("totalprice", 111);
+        expectedData.put("depositpaid", true);
+        expectedData.put("bookingdates", bookingDatesMap);
+        expectedData.put("additionalneeds", "Breakfast");
         System.out.println(expectedData);
 
         //3.Send the request get the response
 
-    Response response= given().spec(spec).when().get("/{first}/{second}");
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        Map<String, Object> actualData = response.as(HashMap.class);
+        //4.Do assertion
 
+        assertEquals(expectedData.get("firstname"),actualData.get("firstname"));
+        assertEquals(expectedData.get("lastname"),actualData.get("lastname"));
+        assertEquals(expectedData.get("totalprice"),actualData.get("totalprice"));
+        assertEquals(expectedData.get("depositpaid"),actualData.get("depositpaid"));
+        assertEquals(bookingDatesMap.get("checkin"),((Map)actualData.get("bookingdates")).get("checkin"));
+        assertEquals(bookingDatesMap.get("checkout"),((Map)actualData.get("bookingdates")).get("checkout"));
+        assertEquals(expectedData.get("additionalneeds"),actualData.get("additionalneeds"));
 
     }
 }
