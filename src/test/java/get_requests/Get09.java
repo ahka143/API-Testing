7e4c6f65556fdf84846c853985ea705d54ef8989
Ahmet Kaya <ahka1143@gmail.com>
2022-07-13 22:51:57 +0300

    13.7.22

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -1,7 +1,13 @@
 package get_requests;
 
 import base_Urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
 
 public class Get09 extends HerOkuAppBaseUrl {
 
@@ -32,7 +38,21 @@ public class Get09 extends HerOkuAppBaseUrl {
         spec.pathParams("first","booking","second",91);
 
         //2.Set the expectedData
+        Map<String,String> bookingDatesMap=new HashMap<>();
+        bookingDatesMap.put("checkin","2018-01-01");
+        bookingDatesMap.put("checkout","2019-01-01");
+        Map<String,Object> expectedData=new HashMap<>();
+        expectedData.put("firstname","James");
+        expectedData.put("lastname","Brown");
+        expectedData.put("totalprice",111);
+        expectedData.put("depositpaid",true);
+        expectedData.put("bookingdates",bookingDatesMap);
+        expectedData.put("additionalneeds","Breakfast");
+        System.out.println(expectedData);
 
+        //3.Send the request get the response
+
+    Response response= given().spec(spec).when().get("/{first}/{second}");
 
 
     }
