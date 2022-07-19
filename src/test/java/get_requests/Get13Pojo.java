84348c6a9ef5b068be2e7422abfcfc25a7012b76
Ahmet Kaya <ahka1143@gmail.com>
2022-07-19 22:57:18 +0300

    19.7.22

--- src/test/java/get_requests/Get13Pojo.java
+++ src/test/java/get_requests/Get13Pojo.java
@@ -1,6 +1,15 @@
 package get_requests;
 
-public class Get13Pojo {
+import base_Urls.GorestBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.GoRestDataPojo;
+import pojos.GoRestResponseBodyPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
+
+public class Get13Pojo extends GorestBaseUrl {
     /*
     Given
             https://gorest.co.in/public/v1/users/2508
@@ -23,4 +32,33 @@ public class Get13Pojo {
             }
 
      */
+
+    @Test
+    public void get01Pojo() {
+
+        //1.step:Set the url
+
+        spec.pathParams("first", "users", "second", 2508);
+
+        //2.step:set the expected data
+
+        GoRestDataPojo goRestDataPojo = new GoRestDataPojo(2508, "Akshita Nehru", "nehru_akshita@jast.info", "female", "active");
+        GoRestResponseBodyPojo goRestResponseBodyPojo = new GoRestResponseBodyPojo(null, goRestDataPojo);
+
+        //3.step:Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //4.step:Do Assertion
+        GoRestResponseBodyPojo actualPojo= response.as(GoRestResponseBodyPojo.class);
+
+        assertEquals(200,response.getStatusCode());
+        assertEquals(goRestResponseBodyPojo.getMeta(),actualPojo.getMeta());
+        assertEquals(goRestDataPojo.getId(),actualPojo.getData().getId());
+        assertEquals(goRestDataPojo.getName(),actualPojo.getData().getName());
+        assertEquals(goRestDataPojo.getEmail(),actualPojo.getData().getEmail());
+        assertEquals(goRestDataPojo.getGender(),actualPojo.getData().getGender());
+        assertEquals(goRestDataPojo.getStatus(),actualPojo.getData().getStatus());
+
+    }
 }
