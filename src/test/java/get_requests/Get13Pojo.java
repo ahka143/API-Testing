7783abfed2635c8ff68a65dae67a742f803c7805
Ahmet Kaya <ahka1143@gmail.com>
2022-07-19 22:08:51 +0300

    19.7.22

--- src/test/java/get_requests/Get13Pojo.java
+++ src/test/java/get_requests/Get13Pojo.java
@@ -1,4 +1,26 @@
 package get_requests;
 
 public class Get13Pojo {
+    /*
+    Given
+            https://gorest.co.in/public/v1/users/2508
+        When
+            User send GET Request to the URL
+        Then
+            Status Code should be 200
+        And
+            Response body should be like
+          {
+    "meta": null,
+    "data": {
+        "id": 2508,
+        "name": "Akshita Nehru",
+        "email": "nehru_akshita@jast.info",
+        "gender": "female",
+        "status": "active"
+    }
+}
+            }
+
+     */
 }
